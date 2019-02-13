32a3dfa676d80f39d44beeff3db37f0ee8121e16
athirapaul <athirapaul24@gmail.com>
2019-02-13 18:10:08 -0500

    Initial Commit

---
+++ day4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day4
+//
+//  Created by Athira Paul on 2019-02-13.
+//  Copyright © 2019 Athira Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
